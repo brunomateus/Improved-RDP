05f5b862739f5761ba2956da82d3821590ffa6f6
Bruno Mateus <brunomateus@gmail.com>
2024-04-25 23:48:29 -0300

    Nova versão com informações da mostura

--- assets/i18n.c4e5c3d3.js
+++ assets/i18n.c4e5c3d3.js
@@ -1,4 +1,4 @@
-import{d as dt,h as zt,e as An,i as Sn,o as Pn,a as yn,c as Oe,w as st,F as en,g as tn,b as Rn,r as Dn,s as kn,f as Mn,T as Fn,j as vn}from"./index.0de2885c.js";/*!
+import{d as dt,h as zt,e as An,i as Sn,o as Pn,a as yn,c as Oe,w as st,F as en,g as tn,b as Rn,r as Dn,s as kn,f as Mn,T as Fn,j as vn}from"./index.fd638cee.js";/*!
   * shared v9.12.1
   * (c) 2024 kazuya kawaguchi
   * Released under the MIT License.
